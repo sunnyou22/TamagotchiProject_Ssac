593ffaecfc99041c7ab1f4cabb6fa501dcd7ac95
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-23 13:49:02 +0900

    feat: 애셋추가, 스토리보드, 뷰컨트롤러 추가

---
+++ TamagotchiProject/firstStart/InitialStartCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  InitialStartCollectionViewCell.swift
+//  TamagotchiProject
+//
+//  Created by 방선우 on 2022/07/23.
+//
+
+import UIKit
+
+class InitialStartCollectionViewCell: UICollectionViewCell {
+    
+}
