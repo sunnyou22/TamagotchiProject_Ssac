6f3b021a615b6ee9f7c3bc55a93ed8d1f1647aa5
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-23 16:09:17 +0900

    feat: 다마고치별 팝업화원의 기본정보 구조체 생성 feat: 다마고치 선택화면 UI구성중

---
+++ TamagotchiProject/TamagotchiIData.swift
@@ -0,0 +1,24 @@
+//
+//  TamagotchiInfo.swift
+//  TamagotchiProject
+//
+//  Created by 방선우 on 2022/07/23.
+//
+
+import Foundation
+
+// 유저 이름 넣어주기
+struct Tamagochi {
+    var name: String
+    var description: String
+    var imageNumber: Int
+   
+}
+
+struct TamagochiInfo {
+    let Tamagochis: [Tamagochi] = [
+        Tamagochi(name: "따끔따끔 다마고치", description: "저는 선인장 다마고치 입니다.\n키는 2cm 몸무케는 150g이에요\n성격은 소심하지만 마음은 따듯해요\n열심히 잘 먹고 잘 클 자신있답니다.\n반가워요 주인님!!!!", imageNumber: 1),
+        Tamagochi(name: "방실방실 다마고치", description: "저는 방실방실 다마고치입니당\n키는 100km 몸무게는 150t에용\n성격은 화끈하고 날라다닙니당~!\n열심히 잘 먹고 잘 클 자신 있답니당 방실방실~", imageNumber: 2),
+        Tamagochi(name: "반짝반짝 다마고치", description: "저는 반짝반짝 다마고치 입니다.\n키는 1000km 몸무케는 비밀이에요(씽긋)\n성격은 낙천적이고 발랄하답니다 *_*\n열심히 잘 먹고 잘 클 자신있답니닷\n주인님과 잘 지내고 싶어요 *O*!", imageNumber: 3)
+        
+}
--- TamagotchiProject/firstStart/InitialStartCollectionViewCell.swift
+++ TamagotchiProject/firstStart/InitialStartCollectionViewCell.swift
@@ -8,5 +8,32 @@
 import UIKit
 
 class InitialStartCollectionViewCell: UICollectionViewCell {
+    static let identifier = "InitialStartCollectionViewCell"
     
+    @IBOutlet weak var labelView: UIView!
+    @IBOutlet weak var staticImage: UIImageView!
+    @IBOutlet weak var staticName: UILabel!
+    
+    func configureCell(imageNumber: Int) {
+        let textAndBoderColor: UIColor = UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+        let backgroundColor: UIColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+        
+        //MARK: labelViewUI
+        labelView.clipsToBounds = true
+        labelView.layer.cornerRadius = 10
+        labelView.layer.borderColor = textAndBoderColor.cgColor
+        labelView.layer.borderWidth = 1.5
+        labelView.layer.backgroundColor = UIColor.clear.cgColor
+        
+        //MARK: staticImage
+        if imageNumber < 4 {
+        staticImage.image = UIImage(named: "\(imageNumber)-6")
+        } else {
+            staticImage.image = UIImage(named: "noImage")
+        }
+        
+    //MARK: staticLabel
+        staticName
+        
+    }
 }
